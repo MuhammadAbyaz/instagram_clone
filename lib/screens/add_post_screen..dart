a63722ab290cb6421d5f5fed149f3d535fed744e
Muhammad Abyaz <abyazkhalid2005@gmail.com>
2023-07-28 20:21:46 +0500

    added select image method

--- lib/screens/add_post_screen..dart
+++ lib/screens/add_post_screen..dart
@@ -1,5 +1,13 @@
+import 'dart:typed_data';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram_clone/models/user.dart';
+import 'package:instagram_clone/providers/user_provider.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/utils.dart';
+import 'package:provider/provider.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -9,71 +17,121 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _file;
+  final TextEditingController _descriptionController = TextEditingController();
+
+  _selectImage(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text("Create a post"),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text("Take a photo"),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(
+                    ImageSource.camera,
+                  );
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text("Choose from gallery"),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(
+                    ImageSource.gallery,
+                  );
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text("Cancel"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return const Center(
-    //   child: IconButton(onPressed: null, icon: Icon(Icons.upload)),
-    // );
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        leading: const IconButton(
-          icon: Icon(Icons.arrow_back_rounded),
-          onPressed: null,
-        ),
-        title: const Text("Post to"),
-        centerTitle: false,
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              "Post",
-              style: TextStyle(
-                color: Colors.blueAccent,
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
+    final UserModel user = Provider.of<UserProvider>(context).getUser;
+
+    return _file == null
+        ? Center(
+            child: IconButton(onPressed: () => _selectImage(context), icon: const Icon(Icons.upload)),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: mobileBackgroundColor,
+              leading: const IconButton(
+                icon: Icon(Icons.arrow_back_rounded),
+                onPressed: null,
               ),
-            ),
-          ),
-        ],
-      ),
-      body: Column(children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://images.unsplash.com/photo-1682686580922-2e594f8bdaa7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.45,
-              child: const TextField(
-                decoration: InputDecoration(hintText: "Write a caption....", border: InputBorder.none),
-                maxLines: 8,
-              ),
-            ),
-            SizedBox(
-              height: 45,
-              width: 45,
-              child: AspectRatio(
-                aspectRatio: 487 / 451,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(
-                          "https://images.unsplash.com/photo-1682686580922-2e594f8bdaa7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"),
-                      fit: BoxFit.fill,
-                      alignment: FractionalOffset.topCenter,
+              title: const Text("Post to"),
+              centerTitle: false,
+              actions: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Post",
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
-            const Divider(),
-          ],
-        )
-      ]),
-    );
+            body: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(user.photoUrl),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    child: TextField(
+                      controller: _descriptionController,
+                      decoration: const InputDecoration(hintText: "Write a caption....", border: InputBorder.none),
+                      maxLines: 8,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 45,
+                    width: 45,
+                    child: AspectRatio(
+                      aspectRatio: 487 / 451,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: MemoryImage(_file!),
+                            fit: BoxFit.fill,
+                            alignment: FractionalOffset.topCenter,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Divider(),
+                ],
+              )
+            ]),
+          );
   }
 }
